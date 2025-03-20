28d874651ff8e5b0e949d82531093dce88ea5f17
Paweł Bihun <pawelbihun@gmail.com>
2025-03-20 08:50:18 +0100

    add boilerplate project files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  console.log('hello world');
+  return (
+    <>
+      <h1>Boilerplate</h1>
+      <h1>Vite + React + TS + SASS + SWC Plugin</h1>
+    </>
+  );
+}
+
+export default App;
